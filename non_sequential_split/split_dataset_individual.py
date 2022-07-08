176ff776606c672f008541462f1b35e8a75e90ee
Annanda Sousa <annanda.sousa@insight-centre.org>
2022-07-08 15:35:37 +0100

    Finishing the split of the dataset per individuals and non-sequential split of 80/10/10 for train/dev/test sets.

--- non_sequential_split/split_dataset_individual.py
+++ non_sequential_split/split_dataset_individual.py
@@ -47,7 +47,10 @@ def separate_annotation_classes(session_number):
 def split_classes_into_sets(session):
     folder = os.path.join(pkl_classes_folder, session)
     classes_files = os.listdir(folder)
+    output_folder = os.path.join(pkl_folder, 'per_class_split', session)
     for class_file in classes_files:
+        emotion = class_file.split('_')[-1]
+        emotion = emotion.replace('.pkl', '')
         df = pd.read_pickle(folder + '/' + class_file)
         df = df.reset_index(drop=True)
         df = df.iloc[:, 1:]
@@ -55,16 +58,62 @@ def split_classes_into_sets(session):
         dev_test_df = df.drop(train_df.index)
         dev_df = dev_test_df.sample(frac=0.5, random_state=25)
         test_df = dev_test_df.drop(dev_df.index)
-        print('yay')
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+        train_df.to_pickle(output_folder + '/' + session + '_' + emotion + '_' + 'train.pkl')
+        dev_df.to_pickle(output_folder + '/' + session + '_' + emotion + '_' + 'dev.pkl')
+        test_df.to_pickle(output_folder + '/' + session + '_' + emotion + '_' + 'test.pkl')
+
+
+def concatenate_sets(session):
+    folder = os.path.join(pkl_folder, 'per_class_split', session)
+    split_folder = os.path.join(pkl_folder, 'split')
+    files_in_folder = os.listdir(folder)
+    train = []
+    test = []
+    dev = []
+    for file in files_in_folder:
+        df_file = pd.read_pickle(folder + '/' + file)
+        if 'train' in file:
+            train.append(df_file)
+            continue
+        elif 'dev' in file:
+            dev.append(df_file)
+            continue
+        elif 'test' in file:
+            test.append(df_file)
+    df_train = pd.concat(train)
+    df_dev = pd.concat(dev)
+    df_test = pd.concat(test)
+    if not os.path.exists(split_folder):
+        os.makedirs(split_folder)
+    output_folder = os.path.join(split_folder, session)
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    df_train.to_pickle(output_folder + '/' + session + '_train.pkl')
+    df_dev.to_pickle(output_folder + '/' + session + '_dev.pkl')
+    df_test.to_pickle(output_folder + '/' + session + '_test.pkl')
+
+
+def main_split(session):
+    separate_annotation_classes(session)
+    split_classes_into_sets(session)
+    concatenate_sets(session)
 
 
 if __name__ == '__main__':
-    list_datasets = ['session_04_02_01.csv',
-                     'session_04_02_02.csv',
-                     'session_04_02_03.csv',
-                     'session_04_02_04.csv',
-                     'session_04_02_05.csv',
-                     ]
+    ###################
+    # 1. If the data of a session is not together into one file:
+
+    # list_datasets = ['session_04_02_01.csv',
+    #                  'session_04_02_02.csv',
+    #                  'session_04_02_03.csv',
+    #                  'session_04_02_04.csv',
+    #                  'session_04_02_05.csv',
+    #                  ]
     # concat_and_save_datasets(list_datasets, session_number='session_04_02')
-    # separate_annotation_classes('session_01_01')
-    split_classes_into_sets('session_01_01')
+
+    ###################
+    # 2. To split into train, dev and test sets
+
+    main_split('session_04_02')
