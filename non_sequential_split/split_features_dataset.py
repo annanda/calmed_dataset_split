85f71f865c818307cc23cc5842bc3e7efcf5cbc9
Annanda Sousa <annanda.sousa@insight-centre.org>
2022-07-18 15:53:13 +0100

    Starting to create the splitting for features too.

---
+++ non_sequential_split/split_features_dataset.py
@@ -0,0 +1,14 @@
+import os.path
+import pandas as pd
+
+from setup.conf import FEATURES_DATASET_DIRECTORY, \
+    VIDEO_FEATURES_DATASET_DIRECTORY
+
+
+def generate_split_per_feature(feature):
+    pass
+
+
+if __name__ == '__main__':
+    feature_name = '2d_eye_landmark'
+    generate_split_per_feature(feature_name)
--- setup/conf.py
+++ setup/conf.py
@@ -6,6 +6,7 @@ from decouple import config
 main_folder = pathlib.Path(__file__).parent.parent.absolute()
 MAIN_FOLDER = config('MAIN_FOLDER', default=main_folder)
 
+# labels
 annotated_dataset_directory = os.path.join(MAIN_FOLDER, 'annotated_dataset')
 pkl_directory = os.path.join(MAIN_FOLDER, 'pkl_datasets')
 pkl_directory_labels = os.path.join(pkl_directory, 'labels')
@@ -19,3 +20,10 @@ PER_CLASS_PKL_DIRECTORY = config('PER_CLASS_PKL_DIRECTORY', default=per_class_pk
 SPLIT_PKL_DIRECTORY = config('SPLIT_PKL_DIRECTORY', default=split_pkl_directory)
 PER_CLASS_SPLIT_PKL_DIRECTORY = config('PER_CLASS_SPLIT_PKL_DIRECTORY', default=per_class_split_pkl_directory)
 PKL_DIRECTORY_LABELS = config('PKL_DIRECTORY_LABELS', default=pkl_directory_labels)
+
+# features
+features_dataset_directory = os.path.join('MAIN_FOLDER', 'feature_dataset')
+video_features_dataset_directory = os.path.join(features_dataset_directory, 'video')
+
+FEATURES_DATASET_DIRECTORY = config('FEATURES_DATASET_DIRECTORY', default=features_dataset_directory)
+VIDEO_FEATURES_DATASET_DIRECTORY = config('VIDEO_FEATURES_DATASET_DIRECTORY', default=video_features_dataset_directory)
