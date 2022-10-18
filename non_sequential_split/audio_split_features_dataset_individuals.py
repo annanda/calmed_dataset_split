9190be6ae6ae95249fab529cf42f38e9f105b43c
Annanda Sousa <annanda.sousa@insight-centre.org>
2022-10-18 09:23:52 +0100

    Adding code to split audio features.

--- non_sequential_split/audio_split_features_dataset_individuals.py
+++ non_sequential_split/audio_split_features_dataset_individuals.py
@@ -1,39 +1,44 @@
 import os.path
 import pandas as pd
 
-from setup.conf import FEATURES_DATASET_DIRECTORY, \
-    VIDEO_FEATURES_DATASET_DIRECTORY, SPLIT_PKL_DIRECTORY, PKL_DIRECTORY_FEATURES_VIDEO
+from non_sequential_split.conf import FEATURES_DATASET_DIRECTORY, \
+    AUDIO_FEATURES_DATASET_DIRECTORY, SPLIT_PKL_DIRECTORY_LABELS, PKL_DIRECTORY_FEATURES_AUDIO, LLD_PARAMETER_GROUP
 
 
-def generate_split_per_feature(session, feature):
+def generate_split_per_feature(session, feature_group, feature_level='functionals'):
     """
     It receives a feature name and generate the split train, dev, test sets for the dataset of the given feature.
-    :param feature:
+    :param feature_group:
     :return:
     """
-    train_label = pd.read_pickle(os.path.join(SPLIT_PKL_DIRECTORY, session, f'{session}_train.pkl'))
-    dev_label = pd.read_pickle(os.path.join(SPLIT_PKL_DIRECTORY, session, f'{session}_dev.pkl'))
-    test_label = pd.read_pickle(os.path.join(SPLIT_PKL_DIRECTORY, session, f'{session}_test.pkl'))
+    train_label = pd.read_pickle(os.path.join(SPLIT_PKL_DIRECTORY_LABELS, session, f'{session}_train.pkl'))
+    dev_label = pd.read_pickle(os.path.join(SPLIT_PKL_DIRECTORY_LABELS, session, f'{session}_dev.pkl'))
+    test_label = pd.read_pickle(os.path.join(SPLIT_PKL_DIRECTORY_LABELS, session, f'{session}_test.pkl'))
 
-    features_folder_read = os.path.join(VIDEO_FEATURES_DATASET_DIRECTORY, feature, session)
-    all_feature_files = os.listdir(features_folder_read)
-    all_feature_files = [file for file in all_feature_files if '.csv' in file]
-    features_folder_write = os.path.join(PKL_DIRECTORY_FEATURES_VIDEO, feature, session)
-    if not os.path.exists(features_folder_write):
-        os.makedirs(features_folder_write)
+    features_of_group = LLD_PARAMETER_GROUP[feature_group]
 
-    for feature_file in all_feature_files:
-        name = feature_file.replace('.csv', '')
-        feature_pd = pd.read_csv(os.path.join(features_folder_read, feature_file))
+    for feature in features_of_group:
+        features_folder_read = os.path.join(AUDIO_FEATURES_DATASET_DIRECTORY, feature_level, feature_group, feature,
+                                            session)
+        all_feature_files = os.listdir(features_folder_read)
+        all_feature_files = [file for file in all_feature_files if '.csv' in file]
+        features_folder_write = os.path.join(PKL_DIRECTORY_FEATURES_AUDIO, feature_level, feature_group, feature,
+                                             session)
+        if not os.path.exists(features_folder_write):
+            os.makedirs(features_folder_write)
 
-        result_train = train_label.merge(feature_pd, on="frametime", how='inner')
-        result_train.to_pickle(os.path.join(features_folder_write, f'{name}_train.pkl'))
+        for feature_file in all_feature_files:
+            name = feature_file.replace('.csv', '')
+            feature_pd = pd.read_csv(os.path.join(features_folder_read, feature_file))
 
-        result_dev = dev_label.merge(feature_pd, on="frametime", how='inner')
-        result_dev.to_pickle(os.path.join(features_folder_write, f'{name}_dev.pkl'))
+            result_train = train_label.merge(feature_pd, on="frametime", how='inner')
+            result_train.to_pickle(os.path.join(features_folder_write, f'{name}_train.pkl'))
 
-        result_test = test_label.merge(feature_pd, on="frametime", how='inner')
-        result_test.to_pickle(os.path.join(features_folder_write, f'{name}_test.pkl'))
+            result_dev = dev_label.merge(feature_pd, on="frametime", how='inner')
+            result_dev.to_pickle(os.path.join(features_folder_write, f'{name}_dev.pkl'))
+
+            result_test = test_label.merge(feature_pd, on="frametime", how='inner')
+            result_test.to_pickle(os.path.join(features_folder_write, f'{name}_test.pkl'))
 
 
 def generate_split_all_video_features():
@@ -46,18 +51,15 @@ def generate_split_all_video_features():
         'session_04_01',
         'session_04_02',
     ]
-    video_features = [
-        '2d_eye_landmark',
-        '3d_eye_landmark',
-        'AU',
-        'face_2d_landmarks',
-        'face_3d_landmarks',
-        'gaze',
-        'head_pose',
+    feature_group = [
+        'frequency',
+        'energy_amplitude',
+        'spectral_balance',
+        'temporal_features'
     ]
 
     for session in sessions:
-        for feature in video_features:
+        for feature in feature_group:
             generate_split_per_feature(session, feature)
 
 
--- non_sequential_split/conf.py
+++ non_sequential_split/conf.py
@@ -12,23 +12,36 @@ pkl_directory = os.path.join(MAIN_FOLDER, 'pkl_datasets')
 pkl_directory_labels = os.path.join(pkl_directory, 'labels')
 per_class_pkl_directory = os.path.join(pkl_directory_labels, 'per_class')
 per_class_split_pkl_directory = os.path.join(pkl_directory_labels, 'per_class_split')
-split_pkl_directory = os.path.join(pkl_directory_labels, 'split')
+split_pkl_directory_labels = os.path.join(pkl_directory_labels, 'split')
 
 ANNOTATED_DATASET_DIRECTORY = config('ANNOTATED_DATASET_DIRECTORY', default=annotated_dataset_directory)
 PKL_DIRECTORY = config('PKL_DIRECTORY', default=pkl_directory)
 PKL_DIRECTORY_LABELS = config('PKL_DIRECTORY_LABELS', default=pkl_directory_labels)
 PER_CLASS_PKL_DIRECTORY = config('PER_CLASS_PKL_DIRECTORY', default=per_class_pkl_directory)
 PER_CLASS_SPLIT_PKL_DIRECTORY = config('PER_CLASS_SPLIT_PKL_DIRECTORY', default=per_class_split_pkl_directory)
-SPLIT_PKL_DIRECTORY = config('SPLIT_PKL_DIRECTORY', default=split_pkl_directory)
+SPLIT_PKL_DIRECTORY_LABELS = config('SPLIT_PKL_DIRECTORY', default=split_pkl_directory_labels)
 
 # features
 features_dataset_directory = os.path.join(MAIN_FOLDER, 'features_dataset')
 video_features_dataset_directory = os.path.join(features_dataset_directory, 'video')
+audio_features_dataset_directory = os.path.join(features_dataset_directory, 'audio')
 
 pkl_directory_features = os.path.join(pkl_directory, 'features')
 pkl_directory_features_video = os.path.join(pkl_directory_features, 'video')
+pkl_directory_features_audio = os.path.join(pkl_directory_features, 'audio')
 
 FEATURES_DATASET_DIRECTORY = config('FEATURES_DATASET_DIRECTORY', default=features_dataset_directory)
 VIDEO_FEATURES_DATASET_DIRECTORY = config('VIDEO_FEATURES_DATASET_DIRECTORY', default=video_features_dataset_directory)
+AUDIO_FEATURES_DATASET_DIRECTORY = config('AUDIO_FEATURES_DATASET_DIRECTORY', default=audio_features_dataset_directory)
 PKL_DIRECTORY_FEATURES = config('PKL_DIRECTORY_FEATURES', default=pkl_directory_features)
 PKL_DIRECTORY_FEATURES_VIDEO = config('PKL_DIRECTORY_FEATURES_VIDEO', default=pkl_directory_features_video)
+PKL_DIRECTORY_FEATURES_AUDIO = config('PKL_DIRECTORY_FEATURES_AUDIO', default=pkl_directory_features_audio)
+
+LLD_PARAMETER_GROUP = {
+    'frequency': ['pitch', 'jitter', 'formant_1-3_frequency', 'formant_1-3_bandwidth'],
+    'energy_amplitude': ['shimmer', 'loudness', 'harmonics-to-noise_ratio'],
+    'spectral_balance': ['alpha_ratio', 'hammarberg_index', 'spectral_slope',
+                         'formant_1-3_relative_energy', 'harmonic_difference_H1–H2',
+                         'Harmonic_difference_H1–A3', 'mfcc_1–4'],
+    'temporal_features': ['temporal_features']
+}
